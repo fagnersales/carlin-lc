76253d6853b2806666494f8c13e52e56d54f3c3e
Fagner Sales <fs.fagnersales@gmail.com>
2023-10-29 11:49:11 -0300

    add reloadCommands file

---
+++ src/reloadCommands.ts
@@ -0,0 +1,13 @@
+import { REST, Routes } from "discord.js"
+import { env } from "./env"
+import { commands } from "./handlers/commands"
+
+export const reloadCommands = async () => {
+  const rest = new REST({ version: '10' }).setToken(env.DISCORD_CLIENT_TOKEN)
+
+  const body = {
+    body: commands.map(command => command.slash.toJSON())
+  }
+
+  await rest.put(Routes.applicationCommands(env.DISCORD_CLIENT_ID), body)
+}
